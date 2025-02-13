144f602ffeb7e376b56b1f9b5ea3fbb483bac95e
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 12:45:12 +0100

    change numbers variable from let to const

--- snack-4/main.js
+++ snack-4/main.js
@@ -2,7 +2,7 @@
 CHiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo
 nell'array */
 
-let numbers =[]
+const numbers =[]
 
 for (let index = 0; index <= 6; index++) {
     insertNumb = Number (prompt("Inserisci un numero. es 1,2,3"))
