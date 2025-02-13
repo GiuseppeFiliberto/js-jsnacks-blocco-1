e8adec8d89d9eea037703be764588bb082660734
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 10:32:55 +0100

    fix const secondnumber

--- snack-1/main.js
+++ snack-1/main.js
@@ -3,7 +3,7 @@ il software stampa il maggiore. */
 
 
 const firstNumber = Number (prompt ("Scrivi un numero [es 1, 2, 3, 4"));
-const secondNumber = Number (prompt) ("Scrivine un altro");
+const secondNumber = Number (prompt ("Scrivine un altro"));
 
 if (firstNumber > secondNumber){
     console.log(firstNumber);
