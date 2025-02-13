33da551626ced5e94250afe63d82019a89dde071
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 12:29:15 +0100

    add sum prompt numbers

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,8 +1,13 @@
 /* il software deve chiedere per 10 volte all'utente di inserire un numero
 il programma stampa la somma di tutti i numeri inseriti. */
 
-for (let index = 1; index <= 10; index++) {
-    const number = Number (prompt("Inserisci un numero es. 1,2,3"));
-    
+let sum = 0
+
+for (let index = 0; index <= 10; index++) {
+    let number = Number (prompt("Inserisci un numero es. 1,2,3"));
+
+    sum = sum += number;
     
 }
+
+console.log(sum);
