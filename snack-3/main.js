bd2e4a1cc5cf4ecdcc89e8da2c533299b2c06575
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 11:56:58 +0100

    add for loop

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,2 +1,8 @@
 /* il software deve chiedere per 10 volte all'utente di inserire un numero
 il programma stampa la somma di tutti i numeri inseriti. */
+
+for (let index = 1; index <= 10; index++) {
+    const number = Number (prompt("Inserisci un numero es. 1,2,3"));
+    
+    
+}
