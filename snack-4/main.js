0072270c76f1797cd3d1a328004862b81943cf01
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-13 12:41:54 +0100

    add empty array + loop for

--- snack-4/main.js
+++ snack-4/main.js
@@ -2,3 +2,16 @@
 CHiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo
 nell'array */
 
+let numbers =[]
+
+for (let index = 0; index <= 6; index++) {
+    insertNumb = Number (prompt("Inserisci un numero. es 1,2,3"))
+
+    if( insertNumb % 2 != 0 ){
+        numbers.push(insertNumb);
+    }
+    else{
+        console.log( `${insertNumb} è un numero pari`);
+        
+    }
+}
